d3a1543e4810314e8b680ddd8e084b79a1d102f2
OttoNyman <7716116@gmail.com>
2021-06-03 15:35:03 +0300

    2-siding svjazivanie added

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -11,7 +11,8 @@ export class CarComponent implements OnInit {
   model!: string;
   colors!: Colors;
   options!: string[];
- 
+  isEdit: boolean = false;
+
   constructor() {}
 
   ngOnInit(): void {
@@ -25,18 +26,58 @@ export class CarComponent implements OnInit {
     };
     this.options = ['ABS', 'Автопилот', 'Автопаркинг'];
   }
-bmwSelect(){
-  this.name = 'BMW';
-    this.speed = 200;
-    this.model = 'M5';
-    this.colors = {
-      car: 'красный',
-      salon: 'синий',
-      wheels: 'серебристый',
-    };
-    this.options = ['ABS', 'Автопилот', 'Автопаркинг'];
-}
 
+  addOpt(option: string) {
+    this.options.unshift(option);
+    return false;
+  }
+
+  deleteOpt(option: string) {
+    for (let i = 0; i < this.options.length; i++) {
+      if (this.options[i] == option) {
+        this.options.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  showEdit() {
+    this.isEdit = !this.isEdit;
+  }
+
+  carSelect(carName: string) {
+    if (carName == 'bmw') {
+      this.name = 'BMW';
+      this.speed = 200;
+      this.model = 'M5';
+      this.colors = {
+        car: 'красный',
+        salon: 'синий',
+        wheels: 'серебристый',
+      };
+      this.options = ['ABS', 'Автопилот', 'Автопаркинг'];
+    } else if (carName == 'tesla') {
+      this.name = 'Tesla';
+      this.speed = 250;
+      this.model = 'Model S';
+      this.colors = {
+        car: 'черный',
+        salon: 'черный',
+        wheels: 'серый',
+      };
+      this.options = ['Автопилот', 'Автопаркинг'];
+    } else if (carName == 'audi') {
+      this.name = 'audi';
+      this.speed = 95;
+      this.model = 'RS8';
+      this.colors = {
+        car: 'белый',
+        salon: 'черный',
+        wheels: 'серебристый',
+      };
+      this.options = ['ABS', 'Автопилот', 'Автопаркинг'];
+    }
+  }
 }
 
 interface Colors {
